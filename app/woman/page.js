ca1682c070da591ae4909b13f5ae3995e77aa0cb
habib <habibbarca2015@gmail.com>
2025-07-12 22:57:09 +0330

    create correct modal for womens

--- app/woman/page.js
+++ app/woman/page.js
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import React, { useState, useEffect } from 'react'
 import Header from '../Header'
 import { motion, AnimatePresence } from 'framer-motion'
+import ProductModal from '../modal/ProductModal'
 
 export default function Page() {
   const [selectedProduct, setSelectedProduct] = useState(null)
@@ -49,8 +50,7 @@ export default function Page() {
         {data.products.map(product => (
           <div
             key={product.id}
-            onClick={() => handleOpenModal(product)}
-            className="border p-4 rounded shadow-2xl cursor-pointer hover:shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
+            className="border p-4 rounded shadow-2xl  hover:shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
           >
             <img
               src={product.thumbnail}
@@ -61,56 +61,23 @@ export default function Page() {
               {product.title}
             </h2>
             <p className="text-gray-700">${product.price}</p>
-            <p className="text-gray-400">{product.description}</p>
+           <div
+              className="bg-black text-[#E6D6C2] w-28 p-2 hover:scale-125 delay-100 duration-300 cursor-pointer relative rounded-md mx-auto"
+              onClick={() => handleOpenModal(product)}
+            >
+              <button className="cursor-pointer">Show Details</button>
+            </div>
           </div>
         ))}
       </div>
 
-      {/* مودال */}
-      <AnimatePresence>
-        {selectedProduct && (
-          <motion.div
-            className="fixed inset-0 bg-black/50 z-50"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            onClick={() => setSelectedProduct(null)} // کلیک روی بک‌دراپ مودال را ببند
-          >
-            <motion.div
-              className="absolute left-1/2 transform -translate-x-1/2 bg-black rounded-full shadow-2xl flex flex-col items-center justify-center text-center p-6 w-80 h-80"
-              style={{ top: scrollY + 100 }}
-              initial={{ scale: 0.7, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 0.7, opacity: 0 }}
-              transition={{ duration: 0.3 }}
-              onClick={e => e.stopPropagation()} // جلوگیری از بسته شدن مودال روی کلیک داخل آن
-            >
-              <button
-                className="absolute top-2 right-3 text-[#D8C3A5] text-xl font-bold cursor-pointer"
-                onClick={() => setSelectedProduct(null)}
-                aria-label="Close Modal"
-              >
-                ×
-              </button>
-              <img
-                src={selectedProduct.thumbnail}
-                alt={selectedProduct.title}
-                className="w-24 h-24 object-contain mb-4 rounded"
-              />
-              <h2 className="text-lg font-bold text-white">
-                {selectedProduct.title}
-              </h2>
-              <p className="text-xs text-[#D8C3A5] mt-1 line-clamp-3 px-2">
-                {selectedProduct.description}
-              </p>
-              <p className="text-md font-semibold mt-2 text-white">
-                {selectedProduct.price} $
-              </p>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+    {selectedProduct && (
+            <ProductModal
+              product={selectedProduct}
+              onClose={() => setSelectedProduct(null)}
+              scrollY={scrollY}
+            />
+          )}
     </div>
   )
 }
